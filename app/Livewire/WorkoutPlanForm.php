34c4b54034785af339af4c90504d20b4201b5ea4
Gabooka19 <huszka.gabor2002@gmail.com>
2024-03-11 20:37:07 +0100

    adding picture upload option to the workout plan

--- app/Livewire/WorkoutPlanForm.php
+++ app/Livewire/WorkoutPlanForm.php
@@ -3,19 +3,38 @@
 namespace App\Livewire;
 
 use App\Models\Exercise;
+use App\Models\WorkoutPlan;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class WorkoutPlanForm extends Component
 {
+
+    use WithFileUploads;
+
     public $title;
     public $description;
     public $difficulty;
-    #[Validate('required|min:2|max:5')]
+    public $image;
     public $numberOfDays = 3;
     public $days = [];
     public $all_exercises;
     public $saveButtonVisible = false;
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|min:20|max:120',
+            'description' => 'required|string|min:50|max:300',
+            'difficulty' => 'required',
+            'numberOfDays' => 'required|min:2|max:5',
+            'image' => 'required|image|max:2048',
+            'days.*.exercises.*.id' => 'required',
+            'days.*.exercises.*.sets' => 'required|number|min:2|max:10',
+            'days.*.exercises.*.reps' => 'required|number|min:5|max:25',
+        ];
+    }
 
     public function mount()
     {
@@ -29,14 +48,30 @@ class WorkoutPlanForm extends Component
 
     public function save()
     {
-        dd($this->days);
+        $this->validate();
+
+        if ($this->image) {
+            $filePath = $this->image->store('workout_plans', 'public');
+        }
+
+        $workoutPlan = new WorkoutPlan();
+        $workoutPlan->title = $this->title;
+        $workoutPlan->image_path = $filePath;
+        $workoutPlan->description = $this->description;
+        $workoutPlan->duration = (int)$this->numberOfDays;
+        $workoutPlan->save();
+
+        foreach ($this->days as $day) {
+            foreach ($day['exercises'] as $exercise) {
+            }
+        }
+
+        // dd($this->days);
     }
 
     public function setDays()
     {
-        $this->validate([
-            'numberOfDays' => 'required|integer|min:2|max:5'
-        ]);
+
         $this->days = [];
         for ($i = 0; $i < $this->numberOfDays; $i++) {
             $this->days[] = [
--- resources/views/livewire/workout-plan-form.blade.php
+++ resources/views/livewire/workout-plan-form.blade.php
@@ -16,7 +16,7 @@
             <form wire:submit.prevent="save">
                 @csrf
                 <div class="relative z-0 flex justify-center mb-5 group">
-                    <input wire:model="title" type="text" name="floating_title" id="floating_title"
+                    <input wire:model.blur="title" type="text" name="floating_title" id="floating_title"
                         class="block py-2.5 px-0 w-2/3 text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-action-color focus:outline-none focus:ring-0 focus:border-action-hover peer"
                         placeholder="" autocomplete="off" required />
                     <label for="floating_title"
@@ -31,7 +31,7 @@
                             Difficulty
                         </label>
                     </div>
-                    <select id="difficulty" wire:model="difficulty"
+                    <select id="difficulty" wire:model.blur="difficulty"
                         class="bg-transparent border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block md:w-1/3 w-full p-2.5 dark:bg-secondary-color dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-action-color dark:focus:border-action-color">
                         <option selected value="beginner">Beginner</option>
                         <option value="intermediate">Intermediate</option>
@@ -42,7 +42,7 @@
                     <label for="days" class="block mb-2 text-2xl font-medium text-gray-900 dark:text-gray-400">
                         Training days
                     </label>
-                    <select id="days" wire:model="numberOfDays"
+                    <select id="days" wire:model.blur="numberOfDays"
                         class="bg-transparent border text-center border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block md:w-44 w-1/2 p-2.5 dark:bg-secondary-color dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-action-color dark:focus:border-action-color">
                         <option selected value="2">2</option>
                         <option selected value="3">3</option>
@@ -76,7 +76,7 @@
                                                 Exercise {{ $addedExerciseIndex + 1 }}
                                             </label>
                                             <select id="exercise"
-                                                wire:model="days.{{ $dayIndex }}.exercises.{{ $addedExerciseIndex }}.id"
+                                                wire:model.blur="days.{{ $dayIndex }}.exercises.{{ $addedExerciseIndex }}.id"
                                                 class="bg-transparent capitalize overflow-y-scroll border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block max-w-fit min-w-fit p-2.5 dark:bg-secondary-color dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-action-color dark:focus:border-action-color">
                                                 <option default value="" hidden>Choose an exercise
                                                 </option>
@@ -92,7 +92,7 @@
                                                 Sets
                                             </label>
                                             <input
-                                                wire:model="days.{{ $dayIndex }}.exercises.{{ $addedExerciseIndex }}.sets"
+                                                wire:model.blur="days.{{ $dayIndex }}.exercises.{{ $addedExerciseIndex }}.sets"
                                                 class="bg-transparent border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-secondary-color dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-action-color dark:focus:border-action-color"
                                                 type="number" min="2" max="10" placeholder="3">
                                         </div>
@@ -102,7 +102,7 @@
                                                 Reps
                                             </label>
                                             <input
-                                                wire:model="days.{{ $dayIndex }}.exercises.{{ $addedExerciseIndex }}.reps"
+                                                wire:model.blur="days.{{ $dayIndex }}.exercises.{{ $addedExerciseIndex }}.reps"
                                                 class="bg-transparent border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-secondary-color dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-action-color dark:focus:border-action-color"
                                                 type="number" min="5" max="25" placeholder="5">
                                         </div>
@@ -127,9 +127,32 @@
                     @endforeach
                 </div>
                 @if ($saveButtonVisible)
+                    <div class="flex flex-col items-center w-full ">
+                        <label class="block my-4 text-2xl font-bold text-gray-900 dark:text-white" for="file_input">
+                            Plan picture
+                        </label>
+                        <div>
+                            <input wire:model='image'
+                                class="block w-full text-sm text-gray-900 border-2 border-gray-300 rounded-lg cursor-pointer md:text-base bg-gray-50 dark:text-gray-200 focus:outline-none dark:bg-secondary-color dark:border-gray-600 dark:placeholder-gray-400"
+                                aria-describedby="file_input_help" id="file_input" accept="image/png, image/jpg"
+                                type="file">
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-400" id="file_input_help">
+                                JPG or PNG
+                            </p>
+                            @if ($image)
+                                <div>
+                                    <img class="block w-16 h-16 mt-5 rounded" src="{{ $image->temporaryUrl() }}"
+                                        alt="plan-image">
+                                </div>
+                            @endif
+                        </div>
+                        <div wire:loading.delay wire:target='image'>
+                            <span class="text-xl text-gray-500 animate-pulse">Uploading...</span>
+                        </div>
+                    </div>
                     <div class="flex justify-center">
                         <button type="submit"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:text-2xl w-full sm:w-auto px-5 py-2.5 text-center dark:bg-action-color dark:hover:bg-action-hover dark:focus:ring-white">
+                            class="text-white bg-blue-700 mb-7 mt-10 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:text-2xl w-full sm:w-auto px-5 py-2.5 text-center dark:bg-action-color dark:hover:bg-action-hover dark:focus:ring-white">
                             Save
                         </button>
                     </div>
